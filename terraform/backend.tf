1648e1c6dfbbc34061ad1b86a2969ada585e1257
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-03-02 18:19:41 +0530

    terraform init

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket = "value"
     key = "java-login-App/terraform.tfstate"
-    region =" us-east-1"
+    region = "us-east-1"
+
+    
     dynamodb_table = "terraform-lock"
   }
 }
