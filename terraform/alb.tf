6d9c30e3aa99d1d7ebebdb72a8af8da1b3b41e38
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-03-03 12:20:27 +0530

    Db issue

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -8,7 +8,7 @@ resource "aws_lb" "app_alb" {
 resource "aws_lb_target_group" "app_tg" {
   port =8080
   protocol = "HTTP"
-  vpc_id = aws_vpc.main.id
+  vpc_id = module.vpc.vpc_id
 }
 
 resource "aws_lb_listener" "listener" {
