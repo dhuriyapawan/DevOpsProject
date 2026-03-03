056872e416209fb999ad3f9ab9bc54fb3a2886d1
pawandhuriya@outlook.com <pawandhuriya@outlook.com>
2026-03-03 12:11:22 +0530

    provider issue

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,12 +1,11 @@
-resource "vpc" "main" {
-  cidr_block ="10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostname = true
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
 
-  tags ={
-    Name = "main-vpc"
-  }
-}
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 }
