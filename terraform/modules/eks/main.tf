d25c60dedcc3e50e6c05e716308f5cf6dcfc78fa
pawan <dhuriya.pawan@gmail.com>
2026-06-01 17:57:17 +0530

    change in tr file

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -114,7 +114,7 @@ resource "aws_iam_role_policy_attachment" "container_registry_policy" {
 resource "aws_iam_role_policy_attachment" "ssm_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
+}   
 
 # =========================
 # EKS NODE GROUP
